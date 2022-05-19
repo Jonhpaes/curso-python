84f73b43786af376256400afb676d9f3812514f7
Jonh Alex <jonhpaz08@gmail.com>
2022-05-19 20:08:46 -0300

    aula 2

---
+++ aula002.py
@@ -0,0 +1,14 @@
+a = 10
+b = 6
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('soma: '+ str (soma))
+#também posso usar a virgúla dpois das aspas, ex: print('soma: ',+ soma)
+
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(resto)
