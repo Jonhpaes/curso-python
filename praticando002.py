c65764acb5ae6c63b46943fd2596866e697365fd
Jonh Alex <jonhpaz08@gmail.com>
2022-05-18 21:45:03 -0300

    Update praticando002.py

--- praticando002.py
+++ praticando002.py
@@ -1,14 +1,15 @@
 a = 7
 b = 8
-multiplicacao = a * b
+multiplicacao1 = a * b
 print('A multiplicação de 7 vezes 8 é igual a',multiplicacao1)
 
 c = 75
 d = 5
-multiplicacao1 = c * d
+multiplicacao2 = c * d
 print('A multiplicação de 75 vezes 8 é igual a',multiplicacao2)
 
 e = a * b
 f = c * d
 soma = e + f
-print('A soma das multiplicações a cima da no valor de',soma)
+print('A soma das multiplicações de cima, da no valor de',soma)
+print('Pronto, obrigado pelo seu tempo, espero ter ajudado você. Volte sempre!')
