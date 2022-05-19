d33940c6f0bb7632eb9ebdd1926602c1acf74c45
Jonh Alex <jonhpaz08@gmail.com>
2022-05-19 20:22:24 -0300

    Update aula002.py

--- aula002.py
+++ aula002.py
@@ -1,14 +1,28 @@
+
 a = 10
-b = 6
+b = 5
 soma = a + b
 subtracao = a - b
 multiplicacao = a * b
 divisao = a / b
 resto = a % b
-print('soma: '+ str (soma))
-#também posso usar a virgúla dpois das aspas, ex: print('soma: ',+ soma)
-
-print(subtracao)
-print(multiplicacao)
-print(divisao)
-print(resto)
+print('soma: {soma}. \nSubtração: {sub}.'
+      '\nMultiplicação: {multiplicacao}.'
+      '\nDivisão: {divisao}.'
+      '\nResto da divisao: {resto}.'
+      .format(soma=soma,
+              sub=subtracao,
+              multiplicacao=multiplicacao,
+              divisao=divisao,
+              resto=resto))
+# print('soma: '+ str (soma))
+# também posso usar a virgúla dpois das aspas, ex: print('soma: ',+ soma)
+#
+# print('subtracao: ' + str(subtracao))
+# print(multiplicacao)
+# print(int(divisao))
+# print(divisao)
+# print(resto)
+# x = 1
+# soma2 = int(x) + 1
+# print(soma2)
