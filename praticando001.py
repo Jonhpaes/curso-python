886fa897ef165d0cd86e3845768f7e8a758f9260
Jonh Alex <jonhpaz08@gmail.com>
2022-05-18 21:32:43 -0300

    praticando

---
+++ praticando001.py
@@ -0,0 +1,6 @@
+print('Contas de matemática abaixo')
+
+a = 380
+b = 178
+soma = a + b
+print('a soma de 380 + 178 é de',soma)
---
+++ praticando002.py
@@ -0,0 +1,14 @@
+a = 7
+b = 8
+multiplicacao = a * b
+print('A multiplicação de 7 vezes 8 é igual a',multiplicacao1)
+
+c = 75
+d = 5
+multiplicacao1 = c * d
+print('A multiplicação de 75 vezes 8 é igual a',multiplicacao2)
+
+e = a * b
+f = c * d
+soma = e + f
+print('A soma das multiplicações a cima da no valor de',soma)
