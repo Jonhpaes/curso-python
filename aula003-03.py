c63893ea8f552db187d32c7c50da76ae58d5fa14
Jonh Alex <jonhpaz08@gmail.com>
2022-05-28 00:13:53 -0300

    Create aula003-03.py

---
+++ aula003-03.py
@@ -0,0 +1,6 @@
+a = int(input('Entre com um valor: '))
+resto = a % 2
+if resto == 0: #usamos dois "=" pois um é para receber receber o valor.
+    print('o número é par')
+else:
+    print('o número é impar')
