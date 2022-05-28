d417b875f5a51e1ed291425a9dbee496e31f3631
Jonh Alex <jonhpaz08@gmail.com>
2022-05-28 00:08:22 -0300

    if, else, elif.

---
+++ aula003-01.py
@@ -0,0 +1,7 @@
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+if a > b:
+    print('o maior número é {}'.format(a))
+else:
+    print('o maior número é {}'.format(b))
+print('Final do programa')
