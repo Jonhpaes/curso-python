551e1d291018f937750da6b71e856adf811613e1
Jonh Alex <jonhpaz08@gmail.com>
2022-06-04 13:50:57 -0300

    Update aula004.py

--- aula004.py
+++ aula004.py
@@ -1,5 +1,17 @@
 #Números sequenciais
+a = int(input('Entre com um número: '))
 
+div = 0
+for x in range(1, a+1):
+    resto = a % x
+    print(x, resto)
+    if resto == 0:
+        div += 1           #" += 1 " é o código simplificado ou seja, estou afirmando que ele recebe o 1 mais ele mesmo.
+                           #div + 1
+if div == 2:
+    print('O número {} é primo'.format(a))
+else:
+    print('O número {} não é primo'.format(a))
 
-for x in range(101):
-    print(x)
+# for x in range(101):
+#     print(x)
