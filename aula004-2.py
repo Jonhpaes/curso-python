898a672f0569102c797b6a8ec5765a2102328bb6
Jonh Alex <jonhpaz08@gmail.com>
2022-06-04 13:57:01 -0300

    Update aula004-2.py

--- aula004-2.py
+++ aula004-2.py
@@ -1,13 +1,11 @@
-a = int(input('Entre com um número: '))
-
-div = 0
-for x in range(1, a+1):
-    resto = a % x
-    print(x, resto)
-    if resto == 0:
-        div += 1           #" += 1 " é o código simplificado ou seja, estou afirmando que ele recebe o 1 mais ele mesmo.
-                           #div + 1
-if div == 2:
-    print('O número {} é primo'.format(a))
-else:
-    print('O número {} não é primo'.format(a))
+for num in ranger(101):
+    div = 0
+    for x in range(1, num):
+        resto = num % x
+        print(x, resto)
+        if resto == 0:
+            div += 1
+    if div == 2:
+        print('O número {} é primo'.format(num))
+    else:
+        print('O número {} não é primo'.format(num))
