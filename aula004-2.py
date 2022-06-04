f4ec425f2e52624fa173fc40f9a3af6f548a4a7f
Jonh Alex <jonhpaz08@gmail.com>
2022-06-04 14:03:32 -0300

    Curso python; Num primos

--- aula004-2.py
+++ aula004-2.py
@@ -1,11 +1,10 @@
-for num in ranger(101):
+a = int(input('Entre com um valor: '))
+for num in range(a+1):
     div = 0
-    for x in range(1, num):
+    for x in range(1, num+1):
         resto = num % x
-        print(x, resto)
+        #print(x, resto)
         if resto == 0:
             div += 1
     if div == 2:
-        print('O número {} é primo'.format(num))
-    else:
-        print('O número {} não é primo'.format(num))
+        print(num)
